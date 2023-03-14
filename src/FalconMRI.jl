570cb99227694c187625a3500f9b362e732f1cfd
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2023-03-14 19:47:54 -0400

    Initial version

--- src/FalconMRI.jl
+++ src/FalconMRI.jl
@@ -1,5 +1,27 @@
 module FalconMRI
 
-# Write your package code here.
+
+using CairoMakie
+using PlyIO
+
+
+# File IO
+export falcon_mesh
+export read_falcon_mesh
+export write_falcon_mesh
+export falcon_cortex
+export read_falcon_cortex
+
+
+# Drawing using Makie
+export draw_two_hemispheres!
+export draw_multiple_fields
+
+
+
+# read/write FALCON meshes
+include("falcon_mesh.jl")
+
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,19 @@ using Test
 
 @testset "FalconMRI.jl" begin
     # Write your tests here.
+    @testset "Reading meshes" begin
+        brain=read_falcon_cortex("input/mni_icbm152_ics_sm_")
+        @test size(brain.LH.V) == (22830, 3)
+        @test size(brain.RH.V) == (22810, 3)
+
+        mktempdir() do tmp
+            write_falcon_mesh(joinpath(tmp,"LH.ply"),brain.LH)
+            write_falcon_mesh(joinpath(tmp,"RH.ply"),brain.RH)
+
+            lh=read_falcon_mesh(joinpath(tmp,"LH.ply"))
+            @test size(lh.V) == (22830, 3)
+            rh=read_falcon_mesh(joinpath(tmp,"RH.ply"))
+            @test size(rh.V) == (22810, 3)
+        end
+    end
 end
